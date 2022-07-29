37cd080df8ec374dcd21c0892cc632bbe969579f
Marcel Müller <neikos@neikos.email>
2022-07-29 15:33:40 +0200

    Add binary to transform TypeDesc. to Markdown
    The binary simply reads from stdin and outputs to stdout.
    It takes as input a JSON representation of a TypeDesc and outputs its
    contents as markdown for further consumption.
    
    Co-authored-by: Matthias Beyer <mail@beyermatthias.de>
    Signed-off-by: Marcel Müller <neikos@neikos.email>

---
+++ src/bin/describe_types.rs
@@ -0,0 +1,44 @@
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+use std::{error::Error, fmt::Display};
+
+use clap::{ArgEnum, Parser};
+use type_description::{render::render_to_markdown, TypeDescription};
+
+#[derive(Debug, ArgEnum, PartialEq, Clone, Copy)]
+enum OutputFormat {
+    Markdown,
+}
+
+impl Display for OutputFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OutputFormat::Markdown => write!(f, "markdown"),
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long, value_parser, default_value_t = OutputFormat::Markdown)]
+    output_format: OutputFormat,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let input: TypeDescription = serde_json::from_reader(std::io::stdin())?;
+
+    match args.output_format {
+        OutputFormat::Markdown => {
+            println!("{}", render_to_markdown(&input)?);
+        }
+    }
+
+    Ok(())
+}
