8702657af004f7592fb767bc28c3cd2bd2ca8a37
Matthias Beyer <mail@beyermatthias.de>
2023-01-16 11:01:49 +0100

    Remove unused import
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     nixpkgs.url = "nixpkgs/nixos-22.05";
     flake-utils = {
       url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     crane = {
       url = "github:ipetkov/crane";
