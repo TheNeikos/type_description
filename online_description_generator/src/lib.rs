b669802d782d99989bf6fd02cb5fdf348bc456c5
Matthias Beyer <mail@beyermatthias.de>
2023-01-25 10:38:03 +0100

    Rewrite for wasm-pack
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- online_description_generator/src/lib.rs
+++ online_description_generator/src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn compile(input: String) -> Result<String, String> {
-    Ok(input.to_uppercase())
+pub fn compile(input: String) -> String  {
+    console_error_panic_hook::set_once();
+
+    input.to_uppercase()
 }
