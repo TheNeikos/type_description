9e5a37dbe20c3705049879a298b8ba12a433078b
Marcel Müller <neikos@neikos.email>
2023-01-27 08:55:15 +0100

    Add license headers
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- online_description_generator/src/main.rs
+++ online_description_generator/src/main.rs
@@ -1,3 +1,8 @@
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 use base64::Engine;
 use console_error_panic_hook::set_once as set_panic_hook;
 use type_description::TypeDescription;
